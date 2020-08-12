1259956874b25b5ed1f21f8e9b5486cd9c76659f
Jpendy <Jpendy256@gmail.com>
2020-08-12 15:29:59 -0700

    fixed tests

--- __tests__/dogs.test.js
+++ __tests__/dogs.test.js
@@ -3,9 +3,11 @@ const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 const Dog = require('../lib/models/Dog');
+const User = require('../lib/models/User');
 
 const request = require('supertest');
 const app = require('../lib/app');
+const { findOne } = require('../lib/models/User');
 
 describe('dogs routes', () => {
   beforeAll(async() => {
@@ -18,8 +20,18 @@ describe('dogs routes', () => {
   });
 
   let dog;
+  const agent = request.agent(app);
   beforeEach(async() => {
+  
+    const res = await agent
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'user0',
+        password: 'password0'
+      });
+
     dog = await Dog.create({
+      user: res.body._id,
       name: 'Leo',
       breed: 'Black Lab',
       temperament: 'Calm',
@@ -32,10 +44,22 @@ describe('dogs routes', () => {
     return mongod.stop();
   });
 
-  it('it creates a new Dog with POST', () => {
-    return request(app)
+  const getLoggedInUser = () => User.findOne({ username: 'user0' });
+  const prepareOne = model => JSON.parse(JSON.stringify(model));
+  const prepareMany = models => models.map(prepareOne);
+
+  const prepare = model => {
+    if(Array.isArray(model)) return prepareMany(model);
+    return prepareOne(model);
+  };
+
+  it('it creates a new Dog with POST', async() => {
+    const user = await getLoggedInUser();
+
+    return agent
       .post('/api/v1/dogs')
       .send({
+        user: user._id,
         name: 'Leo',
         breed: 'Black Lab',
         temperament: 'Calm',
@@ -44,6 +68,7 @@ describe('dogs routes', () => {
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.anything(),
+          user: user.id,
           name: 'Leo',
           breed: 'Black Lab',
           temperament: 'Calm',
@@ -53,43 +78,38 @@ describe('dogs routes', () => {
       });
   });
 
-  it('it gets a list of all dogs with GET', () => {
-    return request(app)
+  it('it gets a list of all dogs with GET', async() => {
+    const user = await getLoggedInUser();
+    const posts = prepare(await Dog.find({ user: user._id }));
+
+    return agent
       .get('/api/v1/dogs')
       .then(res => {
-        expect(res.body).toEqual([{
-          _id: expect.anything(),
-          name: 'Leo',
-          breed: 'Black Lab',
-          temperament: 'Calm',
-          color: 'Black',
-          __v: 0
-        }]);
+        expect(res.body).toEqual(posts);
       });    
   });
 
-  it('it gets a dog by id with GET', () => {
-    return request(app)
+  it('it gets a dog by id with GET', async() => {
+    const dog = prepare(await Dog.findOne());
+
+    return agent
       .get(`/api/v1/dogs/${dog._id}`)
       .then(res => {
-        expect(res.body).toEqual({
-          _id: dog.id,
-          name: 'Leo',
-          breed: 'Black Lab',
-          temperament: 'Calm',
-          color: 'Black',
-          __v: 0
-        });
+        expect(res.body).toEqual(dog);
       });
   });
 
-  it('it updates a dog with PATCH', () => {
-    return request(app)
+  it('it updates a dog with PATCH', async() => {
+    const user = await getLoggedInUser();
+    const dog = prepare(await Dog.findOne({ user: user._id }));
+
+    return agent
       .patch(`/api/v1/dogs/${dog._id}`)
       .send({ temperament: 'Crazy' })
       .then(res => {
         expect(res.body).toEqual({
-          _id: dog.id,
+          user: user.id,
+          _id: dog._id,
           name: 'Leo',
           breed: 'Black Lab',
           temperament: 'Crazy',
@@ -99,18 +119,14 @@ describe('dogs routes', () => {
       });
   });
 
-  it('it deletes a dog by id with DELETE', () => {
-    return request(app)
+  it('it deletes a dog by id with DELETE', async() => {
+    const user = await getLoggedInUser();
+    const dog = prepare(await Dog.findOne({ user: user._id }));
+
+    return agent
       .delete(`/api/v1/dogs/${dog._id}`)
       .then(res => {
-        expect(res.body).toEqual({
-          _id: dog.id,
-          name: 'Leo',
-          breed: 'Black Lab',
-          temperament: 'Calm',
-          color: 'Black',
-          __v: 0
-        });
+        expect(res.body).toEqual(dog);
       });
   });
 });
